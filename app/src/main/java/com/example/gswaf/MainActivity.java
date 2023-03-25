8828f66f63fa7c93a309f17404221e661b59991b
kelli <kellian.kauffmann@outlook.fr>
2023-03-25 19:36:04 +0100

    add drawer on hamburger button

--- app/src/main/java/com/example/gswaf/MainActivity.java
+++ app/src/main/java/com/example/gswaf/MainActivity.java
@@ -14,9 +14,14 @@ import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.Toast;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.widget.Toolbar;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import com.google.android.material.navigation.NavigationView;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -25,16 +30,34 @@ public class MainActivity extends AppCompatActivity {
     // Animations clic
     Animation scaleUp,scaleDown;
 
+    public DrawerLayout drawerLayout;
+    public ActionBarDrawerToggle actionBarDrawerToggle;
+    private NavigationView navigationView;
+
 
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accueil);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.topAppBar);
+        setSupportActionBar(myToolbar);
+
+        drawerLayout = findViewById(R.id.main_drawer_layout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close);
+
+        // pass the Open and Close toggle for the drawer layout listener
+        // to toggle the button
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+        // to make the Navigation drawer icon always appear on the action bar
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         scaleUp = AnimationUtils.loadAnimation(this,R.anim.scale_up);
         scaleDown = AnimationUtils.loadAnimation(this,R.anim.scale_down);
 
     }
+
 
     public void clic(View view){
 
@@ -83,6 +106,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
         MenuInflater inflater= getMenuInflater();
         inflater.inflate(R.menu.top_app_bar,menu);
         return true;
@@ -90,17 +114,38 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()){
-            case R.id.favorite:
-                Toast.makeText(this, "Item favorite selected", Toast.LENGTH_SHORT).show();
-            case R.id.search:
-                Toast.makeText(this, "Item serach selected", Toast.LENGTH_SHORT).show();
-            default:
-                return super.onOptionsItemSelected(item);
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
         }
+        else {
+            switch (item.getItemId()) {
+                case R.id.favorite:
+                    Toast.makeText(this, "Item favorite selected", Toast.LENGTH_SHORT).show();
+                    return true;
+                case R.id.search:
+                    Toast.makeText(this, "Item serach selected", Toast.LENGTH_SHORT).show();
+                    return true;
+                default:
+                    return super.onOptionsItemSelected(item);
+            }
+        }
+    }
 
+    private void configureDrawerLayout(){
+        this.drawerLayout = (DrawerLayout) findViewById(R.id.main_drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+
+    private void configureNavigationView(){
+        this.navigationView = (NavigationView) findViewById(R.id.activity_main_nav_view);
+        navigationView.setNavigationItemSelectedListener((NavigationView.OnNavigationItemSelectedListener) this);
     }
 }
+
+
 
 
 
